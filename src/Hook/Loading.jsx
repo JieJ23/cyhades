e109b858f6a8a7958b2be5a469e27071b2c51920
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-09-29 11:44:35 -0400

    Replaced Loading

--- src/Hook/Loading.jsx
+++ src/Hook/Loading.jsx
@@ -1,8 +1,10 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center gap-2">
-      <span className="loading loading-spinner loading-lg" />
-      <div>Calling Hades's Office</div>
+    <div className="flex justify-center items-center gap-2 mt-10">
+      <span className="loading loading-ring loading-lg" />
+      <div className="font-customDress text-white text-[18px] uppercase">
+        Locating Underworld Data
+      </div>
     </div>
   );
 }
