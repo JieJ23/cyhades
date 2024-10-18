9ddfdb33a45ae40686ef911215e11da3c61bbe91
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-18 12:58:24 -0400

    Template Check

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,13 @@ export default function App() {
           <div className="mb-5 text-4xl font-bold font-serif text-gray-300">
             Supergiant Games
           </div>
-          <div className="max-w-[1200px] mx-auto">
+          <div>
             <div className="my-5">
               <Link
                 to={`/Hades2`}
                 className="group w-full relative group overflow-hidden rounded-xl"
               >
-                <div className="w-full min-h-[220px] max-w-[1000px] mx-auto relative bg-transparent shadow-[inset_0_0_5px_black] border-[1px] border-black/30 rounded-xl">
+                <div className="w-full min-h-[220px] max-w-[800px] mx-auto relative bg-transparent shadow-[inset_0_0_5px_black] border-[1px] border-black/30 rounded-xl">
                   <div
                     className="absolute bg-[#29975399] h-full w-full top-0 left-0 opacity-20 bg-cover bg-center bg-no-repeat rounded-xl group-hover:opacity-40 transition-all ease-in duration-200"
                     style={{ backgroundImage: `url("/hades2BG.png")` }}
@@ -46,20 +46,81 @@ export default function App() {
                   </div>
                 </div>
               </Link>
+              <div className="flex justify-center gap-2">
+                <Link to={`/Hades2`}>
+                  <button className="btn btn-success my-3 btn-outline font-serif">
+                    Hades 2
+                  </button>
+                </Link>
+                <Link
+                  to={`https://www.supergiantgames.com/games/hades-ii/`}
+                  target="_blank"
+                >
+                  <button className="btn btn-success my-3 btn-outline font-serif">
+                    Official SG Hade 2
+                  </button>
+                </Link>
+              </div>
+
+              <div className="font-customDress text-[14px] max-w-[800px] mx-auto text-center text-[#51b084] px-4">
+                Hades II is the upcoming sequel to Hades, developed by
+                Supergiant Games. In this game, you play as Melinoë, the sister
+                of Zagreus and another child of Hades. She embarks on her own
+                journey to confront Chronos, the Titan of Time, who has escaped
+                the Underworld. Like the first game, Hades II features
+                fast-paced, roguelike combat with new weapons, powers, and boons
+                from gods of Greek mythology. The game promises a blend of
+                challenging gameplay, character-driven storytelling, and rich
+                lore, building on the success of its predecessor.
+              </div>
               <div className="rounded-xl">
                 <Marquee className="rounded-xl">
-                  <div className="w-[500px] md:w-[800px] lg:w-[1200px]">
+                  <div className="w-[500px] md:w-[800px] lg:w-[1000px]">
                     <TopPlayers2 />
                   </div>
                 </Marquee>
               </div>
+              <div className="mt-5 flex justify-center gap-2">
+                <Link
+                  to={`https://store.steampowered.com/app/1145350/Hades_II/`}
+                  target="_blank"
+                >
+                  <button className="btn btn-outline text-white font-serif border-white/20">
+                    <div className="avatar">
+                      <div className="w-8 rounded">
+                        <img src="steam.png" />
+                      </div>
+                    </div>
+                    <div>Steam</div>
+                  </button>
+                </Link>
+                <Link
+                  to={`https://store.epicgames.com/en-US/p/hades-ii`}
+                  target="_blank"
+                >
+                  <button className="btn btn-outline text-white font-serif border-white/20">
+                    <div className="avatar">
+                      <div className="w-8 rounded">
+                        <img src="epic.png" />
+                      </div>
+                    </div>
+                    <div>Epic Games</div>
+                  </button>
+                </Link>
+              </div>
             </div>
+            <div className="divider my-10 px-4 divider-neutral">
+              <div className="w-[200px]">
+                <img src={`/logo.png`} />
+              </div>
+            </div>
+
             <div className="my-5">
               <Link
                 to={`/Hades`}
                 className="group w-full overflow-hidden rounded-xl"
               >
-                <div className="w-full min-h-[220px] relative max-w-[1000px] mx-auto bg-transparent shadow-[inset_0_0_5px_black] border-[1px] border-black/30 rounded-xl">
+                <div className="w-full min-h-[220px] relative max-w-[800px] mx-auto bg-transparent shadow-[inset_0_0_5px_black] border-[1px] border-black/30 rounded-xl">
                   <div
                     className="absolute bg-[transparent] h-full w-full top-0 opacity-20 left-0 bg-cover bg-center bg-no-repeat rounded-xl group-hover:opacity-40 transition-all ease-in duration-200"
                     style={{ backgroundImage: `url("/hadesBG.png")` }}
@@ -76,49 +137,73 @@ export default function App() {
                   </div>
                 </div>
               </Link>
+              <div className="flex justify-center gap-2">
+                <Link to={`/Hades`}>
+                  <button className="btn btn-error my-3 btn-outline font-serif">
+                    Hades
+                  </button>
+                </Link>
+                <Link
+                  to={`https://www.supergiantgames.com/games/hades/`}
+                  target="_blank"
+                >
+                  <button className="btn btn-error my-3 btn-outline font-serif">
+                    Official SG Hades
+                  </button>
+                </Link>
+              </div>
+              <div className="font-customDress text-[14px] max-w-[800px] mx-auto text-center text-[#c04747] px-4">
+                Hades is a roguelike action game where you play as Zagreus, the
+                son of Hades, trying to escape the Underworld. Each run is
+                procedurally generated, featuring fast-paced combat with a
+                choice of weapons and powerful boons granted by Olympian gods
+                like Zeus, Athena, and Poseidon. The game blends challenging
+                gameplay with a deep, evolving narrative, offering engaging
+                character interactions and a unique art style. Dying is part of
+                the loop, and each attempt to escape brings new dialogue,
+                upgrades, and story progression.
+              </div>
               <Marquee>
-                <div className="w-[500px] md:w-[800px] lg:w-[1200px]">
+                <div className="w-[500px] md:w-[800px] lg:w-[1000px]">
                   <TopPlayers />
                 </div>
               </Marquee>
             </div>
+            <div className="mt-5 flex justify-center gap-2">
+              <Link
+                to={`https://store.steampowered.com/app/1145360/Hades/`}
+                target="_blank"
+              >
+                <button className="btn btn-outline text-white font-serif border-white/20">
+                  <div className="avatar">
+                    <div className="w-8 rounded">
+                      <img src="steam.png" />
+                    </div>
+                  </div>
+                  <div>Steam</div>
+                </button>
+              </Link>
+              <Link
+                to={`https://store.epicgames.com/en-US/p/hades`}
+                target="_blank"
+              >
+                <button className="btn btn-outline text-white font-serif border-white/20">
+                  <div className="avatar">
+                    <div className="w-8 rounded">
+                      <img src="epic.png" />
+                    </div>
+                  </div>
+                  <div>Epic Games</div>
+                </button>
+              </Link>
+            </div>
+            <div className="divider my-10 px-4 divider-neutral">
+              <div className="w-[200px]">
+                <img src={`/logo2.png`} />
+              </div>
+            </div>
           </div>
         </div>
-        {/* <Link to={`/FormSubmission`}>
-          <button className="btn btn-success">Submission Btn</button>
-        </Link> */}
-        {/*  */}
-        {/* <section className="px-4 flex justify-center">
-          <div className="mockup-code bg-neutral w-full max-w-[800px] mx-auto my-5 p-5 text-black text-start">
-            <pre data-prefix="$" className="text-primary">
-              <code>Documentation</code>
-            </pre>
-            <pre data-prefix="$" className="text-primary">
-              <code>Excel Sheet Monitor</code>
-            </pre>
-            <pre data-prefix="-" className="text-warning">
-              <code>Path /Hades & /Hades2</code>
-            </pre>
-            <pre data-prefix="-" className="text-warning">
-              <code>API GET - H1 / H2</code>
-            </pre>
-            <pre data-prefix="-" className="text-warning">
-              <code>API Post - Dynamic DB/Form</code>
-            </pre>
-            <pre data-prefix="-" className="text-error">
-              <code>H1 - Weap/Aspect/Player</code>
-            </pre>
-            <pre data-prefix="-" className="text-error">
-              <code>H1 Secondary - AOTW: H/T</code>
-            </pre>
-            <pre data-prefix="-" className="text-success">
-              <code>H2 - Weap/Aspect/Player</code>
-            </pre>
-            <pre data-prefix="-" className="text-success">
-              <code>H2 Secondary - Boon/Time</code>
-            </pre>
-          </div>
-        </section> */}
         <FooterInfo />
       </div>
     </div>
